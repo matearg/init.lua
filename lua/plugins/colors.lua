6784f16e084139d48f9d7b2d15b83f0b5d689220
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2025-12-20 23:11:15 -0300

    Removed transparency settings

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -4,6 +4,12 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true, -- disables setting the background color.
+        float = {
+          transparent = true, -- enable transparent floating windows
+        },
+      })
       vim.cmd.colorscheme("catppuccin")
     end,
   },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
-      require("mini.statusline").setup({})
+      -- require("mini.statusline").setup({})
       require("mini.basics").setup({
         mappings = {
           basic = true,
