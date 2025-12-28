a08d2cd782887184d9d7563ea67499169f5870c7
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2025-12-28 09:42:31 -0300

    Lazyvim

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
-require("config.options")
-require("config.keymaps")
-require("config.plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,27 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
 local set = vim.keymap.set
-
-vim.g.mapleader = " "
-vim.g.localmapleader = " "
-
 set("i", "jk", "<ESC>")
 set("i", "<C-h>", "<Left>")
 set("i", "<C-j>", "<Down>")
 set("i", "<C-k>", "<Up>")
 set("i", "<C-l>", "<Right>")
-
--- set("n", "-", vim.cmd.Ex)
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
 set("n", "n", "nzzzv")
 set("n", "N", "Nzzzv")
-set("n", "<S-l>", vim.cmd.bn)
-set("n", "<S-h>", vim.cmd.bp)
--- set("n", "<C-h>", "<C-w>h")
--- set("n", "<C-j>", "<C-w>j")
--- set("n", "<C-k>", "<C-w>k")
--- set("n", "<C-l>", "<C-w>l")
 set("n", "Q", vim.cmd.bdelete)
--- set("n", "<C-w>", vim.cmd.write)
 set("n", "<C-c>", "<cmd> %y+ <cr>")
-
 set("x", "p", '"_dP')
